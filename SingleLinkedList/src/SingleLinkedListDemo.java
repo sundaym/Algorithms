5cedfa5c32d433428649f6342c410d4430dfd601
sundaym <sunday2011s@gmail.com>
2020-09-13 12:11:07 +0800

    链表中间插入节点;链表删除指定节点

--- SingleLinkedList/src/SingleLinkedListDemo.java
+++ SingleLinkedList/src/SingleLinkedListDemo.java
@@ -1,3 +1,5 @@
+import java.beans.DesignMode;
+
 /**
  * 单向链表示例
  */
@@ -15,10 +17,25 @@ public class SingleLinkedListDemo {
         singleLinkedList.show();
         System.out.println("向链表中添加Node");
         // 加入
-        singleLinkedList.add(node1);
-        singleLinkedList.add(node2);
-        singleLinkedList.add(node3);
-        singleLinkedList.add(node4);
+//        singleLinkedList.add(node1);
+//        singleLinkedList.add(node2);
+//        singleLinkedList.add(node3);
+//        singleLinkedList.add(node4);
+//        singleLinkedList.show();
+
+        // 按照编号的顺序加入
+        singleLinkedList.addByOrder(node1);
+        singleLinkedList.addByOrder(node4);
+        singleLinkedList.addByOrder(node3);
+        singleLinkedList.addByOrder(node2);
+        singleLinkedList.show();
+
+        Node newNode = new Node(1, "update data1_0", "update data1_1");
+        singleLinkedList.update(newNode);
+        System.out.println("-------------------------");
+        singleLinkedList.show();
+        System.out.println("-------------------------");
+        singleLinkedList.removeElement(1);
         singleLinkedList.show();
     }
 }
@@ -74,14 +91,103 @@ class SingleLinkedList {
             temp = temp.next;
         }
     }
+
+    /**
+     * 第二种添加方式
+     * @param node
+     */
+    public void addByOrder(Node node) {
+        // 头节点不动，通过辅助变量来找到添加的位置
+        // 因为是单链表，因此我们找的temp是位于添加位置的前一个节点，否则插入不了
+        Node temp = head;
+        boolean flag = false; // 添加的编号是否存在，默认为false
+        while (true) {
+            if (temp.next == null) { // 说明temp已经在链表的最后
+                break;
+            }
+            if (temp.next.no > node.no) {
+                break;
+            } else if (temp.next.no == node.no) {
+                flag = true; // 说明编号存在
+                break;
+            }
+            temp = temp.next; // 后移
+        }
+
+        // 判断flag的值
+        if (flag) { // 不能添加，说明编号存在
+            System.out.println("准备插入的节点的编号存在，不能加入");
+        } else {
+            // 插入链表中，temp后面
+            node.next = temp.next;
+            temp.next = node;
+        }
+    }
+
+    /**
+     * 修改节点的信息，根据no编号修改
+     * @param newNode
+     */
+    public void update(Node newNode) {
+        if (head.next == null) {
+            System.out.println("链表为空");
+            return;
+        }
+        // 找到需要修改的节点
+        Node temp = head.next;
+        boolean flag = false; // 表示是否找到该节点
+        while (true) {
+            if (temp == null) {
+                break; // 已经遍历完链表
+            }
+            if (temp.no == newNode.no) {
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        // 根据flag判断是否已经找到需要修改的节点
+        if (flag) {
+            temp.data0 = newNode.data0;
+        } else {
+            System.out.println("没找到需要修改的节点，不能修改，编号：" + newNode.no);
+        }
+    }
+
+    /**
+     * 1.head不动，使用辅助变量temp找到要删除节点的前一个节点
+     * 2。比较的时候，是temp.next.no和需要需要删除的节点的no比较
+     * @param no
+     */
+    public void removeElement(int no) {
+        Node temp = head;
+        boolean flag = false; // 标识是否找到删除节点
+        while (true) {
+            if (temp.next == null) { // 已经到链表的最后
+                break;
+            }
+            if (temp.next.no == no) {
+                // 找到了删除节点的前一个节点
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+
+        if (flag) {
+            temp.next = temp.next.next;
+        } else {
+            System.out.println("没有找到要删除的节点");
+        }
+    }
 }
 
 /**
  * 定义节点， 每个Node对象就是一个节点
  */
 class Node {
-    private int no;
-    private String data0; // 节点中的数据
+    public int no;
+    public String data0; // 节点中的数据
     private String data1; // 节点中的数据
     public Node next; // 指向下一个节点
 
